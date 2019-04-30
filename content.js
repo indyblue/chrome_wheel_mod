6b804560e422f0b34309cb0e8024d56f389430dd
DAS <david@indyblue.com>
2019-04-30 16:17:34 -0400

    iframe support

--- content.js
+++ content.js
@@ -1,6 +1,31 @@
-window.addEventListener('wheel', function(e) {
-  if (e.altKey === true) {
-    e.preventDefault();
-    return false;
+(function() {
+  function fnWindowWheelZoomNoScroll(e) {
+    if (e.altKey === true) {
+      console.log('zoom, no scroll!!!');
+      e.preventDefault();
+      return false;
+    }
   }
-}, { passive: false });
+  window.addEventListener('wheel',
+    fnWindowWheelZoomNoScroll, { passive: false });
+  for (var i = 0; i < window.frames.length; i++) {
+    window.frames[i].addEventListener('wheel',
+      fnWindowWheelZoomNoScroll, { passive: false });
+  }
+
+  var obs = new MutationObserver(function(mutations, observer) {
+    for (var i = 0; i < mutations.length; ++i) {
+      for (var j = 0; j < mutations[i].addedNodes.length; ++j) {
+        var node = mutations[i].addedNodes[j]
+        if (node.nodeName == "IFRAME") {
+          console.log("iframe added");
+          node.contentWindow.addEventListener('wheel',
+            fnWindowWheelZoomNoScroll, { passive: false });
+        }
+      }
+    }
+  });
+
+  obs.observe(document.body, { childList: true });
+
+})();
